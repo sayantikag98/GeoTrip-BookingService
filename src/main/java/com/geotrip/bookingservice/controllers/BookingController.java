eb3e96ac9b5838fd427dd5699c242d523d018b21
Sayantika Ghosh <sayantika.ghosh@insideiim.com>
2025-03-14 20:05:55 +0530

    Add: Role based access control

--- src/main/java/com/geotrip/bookingservice/controllers/BookingController.java
+++ src/main/java/com/geotrip/bookingservice/controllers/BookingController.java
@@ -2,14 +2,11 @@ package com.geotrip.bookingservice.controllers;
 
 import com.geotrip.bookingservice.dtos.BookingDto;
 import com.geotrip.bookingservice.dtos.CreateBookingRequestDto;
-import com.geotrip.bookingservice.dtos.UserDto;
-import com.geotrip.bookingservice.clients.AuthServiceClient;
 import com.geotrip.bookingservice.services.BookingServiceImpl;
-import com.geotrip.entityservice.models.Role;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,15 +18,11 @@ import java.util.UUID;
 public class BookingController {
 
     private final BookingServiceImpl bookingService;
-    private final AuthServiceClient authServiceClient;
 
     @PostMapping
-    public ResponseEntity<BookingDto> createBooking(@RequestHeader("Authorization") String token, @RequestBody @Valid CreateBookingRequestDto createBookingRequestDto) {
-        UserDto userDto = authServiceClient.validateToken(token).getBody();
-        if(userDto == null || userDto.getRole() != Role.PASSENGER) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-        BookingDto bookingDto = bookingService.createBooking(userDto, createBookingRequestDto);
+    @PreAuthorize("hasAuthority('ROLE_PASSENGER')")
+    public ResponseEntity<BookingDto> createBooking(@RequestBody @Valid CreateBookingRequestDto createBookingRequestDto) {
+        BookingDto bookingDto = bookingService.createBooking(createBookingRequestDto);
         return ResponseEntity.ok(bookingDto);
     }
 
@@ -40,26 +33,28 @@ public class BookingController {
     }
 
     @GetMapping("/passenger/{passengerId}")
+    @PreAuthorize("hasAuthority('ROLE_PASSENGER')")
     public ResponseEntity<List<BookingDto>> getAllBookingsByPassengerId(@PathVariable UUID passengerId) {
         List<BookingDto> bookingDtoList = bookingService.getAllBookingsByPassengerId(passengerId);
         return ResponseEntity.ok(bookingDtoList);
     }
 
     @GetMapping("/driver/{driverId}")
+    @PreAuthorize("hasAuthority('ROLE_DRIVER')")
     public ResponseEntity<List<BookingDto>> getAllBookingsByDriverId(@PathVariable UUID driverId) {
         List<BookingDto> bookingDtoList = bookingService.getAllBookingsByDriverId(driverId);
         return ResponseEntity.ok(bookingDtoList);
     }
 
-    @GetMapping("/available")
-    public ResponseEntity<List<BookingDto>> getAvailableBookings() {
-        List<BookingDto> bookingDtoList = bookingService.getAllAvailableBookings();
-        return ResponseEntity.ok(bookingDtoList);
-    }
-
-    @PatchMapping("/{bookingId}/driver/assign")
-    public ResponseEntity<BookingDto> assignBookingToDriver(@PathVariable UUID bookingId) {
-        BookingDto bookingDto = bookingService.assignBookingToDriver(bookingId);
-        return ResponseEntity.ok(bookingDto);
-    }
+//    @GetMapping("/available")
+//    public ResponseEntity<List<BookingDto>> getAvailableBookings() {
+//        List<BookingDto> bookingDtoList = bookingService.getAllAvailableBookings();
+//        return ResponseEntity.ok(bookingDtoList);
+//    }
+//
+//    @PatchMapping("/{bookingId}/driver/assign")
+//    public ResponseEntity<BookingDto> assignBookingToDriver(@PathVariable UUID bookingId) {
+//        BookingDto bookingDto = bookingService.assignBookingToDriver(bookingId);
+//        return ResponseEntity.ok(bookingDto);
+//    }
 }
