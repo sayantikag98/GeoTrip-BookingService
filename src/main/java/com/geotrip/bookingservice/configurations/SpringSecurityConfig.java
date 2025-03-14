c36ac080b20658e905d81e8f26741fb1bae5a59a
Sayantika Ghosh <sayantika.ghosh@insideiim.com>
2025-03-14 20:02:54 +0530

    Update: Auth filter and security context

--- src/main/java/com/geotrip/bookingservice/configurations/SpringSecurityConfig.java
+++ src/main/java/com/geotrip/bookingservice/configurations/SpringSecurityConfig.java
@@ -1,23 +1,26 @@
 package com.geotrip.bookingservice.configurations;
 
 
+import com.geotrip.bookingservice.filters.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
